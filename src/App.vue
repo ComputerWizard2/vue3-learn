8220ed54a3bee11a207a57795e8d6bb2b1834f4a
luckye <9312486+luckye520@user.noreply.gitee.com>
2024-08-24 10:42:22 +0800

    init vue3-learn

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+<!-- html -->
+ <div class="app">
+  <h1> Hello world</h1>
+ </div>
+</template>
+<script>
+export default{
+    name:"App"
+}
+// js 
+</script>
+<style>
+/* style */
+.app{
+    background-color: aliceblue;
+    box-shadow: 0 0 10px;
+    border-radius: 20px;
+    padding: 20px;
+}
+</style>
