de4aa7b1059699e30479ca87130fad6b46beee75
luckye <9312486+luckye520@user.noreply.gitee.com>
2024-08-25 22:39:04 +0800

    [issue-11] parent component transfor data to child component by props

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
-  <Person/>
+  <Person a="test" :list="personList"/>
 </template>
-<script setup name="App">
-  import Person  from './Component/Person.vue';
+<script setup name="App" lang="ts">
+  import Person  from '@/Component/Person.vue';
+  import {reactive} from 'vue'
+  import {type Persons} from '@/typys'
+  let personList = reactive<Persons>([
+   {id:'1',name:'luck',age:20},
+   {id:'2',name:'luck2',age:21},
+   {id:'3',name:'luck3',age:22}
+  ]);
 </script>
 <style>
 /* style */
--- src/Component/Person.vue
+++ src/Component/Person.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="person">
-    ???
+    <div>{{ a }}</div>
+    <ul>
+      <li v-for="person in list" :key="person.id">{{ person.name }} ~{{ person.age }}~{{ person.id }}</li>
+    </ul>
   </div>
 </template>
 <script setup name="Person" lang="ts">
-import { type PersonInter,type Persons} from '@/typys';
-//let person:PersonInter = {id:'1',name:'Luck',age:11};
-let personList:Persons=[
-  {id:"1",name:"luck1",age:60},
-  {id:"2",name:"luck2",age:60},
-  {id:"3",name:"luck3",age:60},
-]
+import {defineProps,withDefaults} from 'vue'
+import {type Persons} from '@/typys'
+// 只接收list
+//let x = defineProps(['a','list'])
+//接收 list + 限制类型
+//defineProps<{list:Persons}>();
+// 接收list + 限制类型+限制必要性 + 指定默认值
+// withDefaults(defineProps<{list?:Persons}>(),{
+//   list:()=>[{id:'4',name:'luck5',age:50}]
+// })
 </script>
 <style>
 .person{
