c09337e08d8205396e7a8ed070ce6da8d0f2466d
luckye <9312486+luckye520@user.noreply.gitee.com>
2024-08-25 09:00:54 +0800

    [issue-9] reactive wrapper  watch

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -5,28 +5,39 @@
   </div> 
   <button @click="changeName">change Name</button> 
   <button @click="changePerson">change Person</button> 
+  <hr>
+  <div>{{ obj.a.b.c }}</div>
+  <button @click="changeDeepProp">change deep prop</button>
 </template>
 <!-- install vue plugin fsr set up :this is not define vue component name -->
  <!--npm i vite-plugin-vue-setup-extend -D  -->
  <!--then config the vite.config.ts  -->
 <script setup name="Person223">
 // structure data still is reactive data by wrapper toRefs
-import { watch, ref } from 'vue';
-let person = ref({
+import { watch, reactive } from 'vue';
+let person = reactive({
   name:'Jolhe',
   age:12
 });
+let obj = reactive({a:{b:{c:666}}})
 function changeName() {
-  person.value.name += "~";
+  person.name += "~";
 }
 function changePerson(){
-  console.log(person.value) 
-  person.value={name:'Marry', age:22}
+  person=Object.assign(person,{name:'Marry', age :22})
 }
+function changeDeepProp(){
+  obj.a.b.c=888
+}
+
 watch(person, (newValue, oldValue) => {
   console.log(`new value: ${JSON.stringify(newValue)}, old value: ${JSON.stringify(oldValue)}`);
-},{deep:true,immediate:true});
+} );
 
+// Implicit listening that force to open and not close
+watch(obj,(newValue,oldValue)=>{
+  console.log('obj changed ',newValue,oldValue);
+})
 </script>
 <style scoped>
 .person{
